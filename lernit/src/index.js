2058973ab26f018cdb08d6b9808bf49452e1e736
Enrique García <garcia.enrique.torres@gmail.com>
2020-05-22 13:03:45 +0200

    Add to Login initial page

--- lernit/src/index.js
+++ lernit/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import Login from './components/Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
