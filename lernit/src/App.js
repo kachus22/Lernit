1d5d847a127909ecee1998cb7c6d1147f104f335
Enrique García <garcia.enrique.torres@gmail.com>
2020-05-22 19:48:10 +0200

    Use firestorage to get realtime updates on counter

--- lernit/src/App.js
+++ lernit/src/App.js
@@ -7,6 +7,29 @@ import './App.css';
 class App extends Component {
   static contextType = FirebaseContext
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  componentDidMount() {
+    const that = this;
+    this.context.fb.auth.onAuthStateChanged(user => {
+      // Using realtime database
+      // const ref = this.context.fb.counterByUID(user.uid)
+      // ref.on('value', (snap) => {
+      //   that.setState({ counter: snap.val().counter });
+      // });
+      // Using firestorage
+      const ref = that.context.fb.counterByUID(user.uid)
+      ref.onSnapshot((doc) => {
+        that.setState({ counter: doc.data().counter });
+      });
+    });
+  }
+
   logout() {
     this.context.fb.logout()
       .then((res) => {
@@ -23,16 +46,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload {this.state.counter}.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button variant="plus-lernit" className="transition-3d-hover" onClick={() => this.plusOne()}>+</Button>
         </header>
         <div className="actions">
           <Button variant="primary-lernit" className="transition-3d-hover" onClick={() => this.logout()}>Cerrar sesión</Button>
