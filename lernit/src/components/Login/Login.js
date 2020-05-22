08ee492655a7eab424a40de79500ec40b3ad2dc8
Enrique García <garcia.enrique.torres@gmail.com>
2020-05-22 15:53:49 +0200

    Add form validation in Login

--- lernit/src/components/Login/Login.js
+++ lernit/src/components/Login/Login.js
@@ -1,19 +1,80 @@
 import React, { Component } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { FirebaseContext } from '../../firebase';
 import './Login.css';
 
+const defaultFormValues = {
+  email: '',
+  password: ''
+}
+
 export default class Login extends Component {
+  static contextType = FirebaseContext
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: { ...defaultFormValues },
+      alert: {
+        message: '',
+        show: false,
+        variant: 'danger'
+      }
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let fieldName = event.target.name;
+    let fleldVal = event.target.value;
+    this.setState({form: {...this.state.form, [fieldName]: fleldVal}})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const form = event.currentTarget;
+    if (form.checkValidity()) {
+      this.context.login(this.state.form.email, this.state.form.password)
+      .then((res) =>{
+        console.log(res); // TODO: Redirect to home
+      })
+      .catch((err) => {
+        if (err.code === 'auth/email-already-in-use') {
+          this.showAlert('Ya existe una cuenta con este correo electrónico', 'warning');
+        }
+      })
+    } else {
+      this.showAlert('Revisa bien tus datos', 'danger');
+    }
+  }
+
+  showAlert(message, variant) {
+    this.setState({alert: { message: message, show: true, variant: variant }})
+    setTimeout(() => {
+      this.setState({alert: { ...this.state.alert, show: false }})
+    }, 2500);
+  }
+
   render() {
+    const showAlert = this.state.alert.show;
+
     return (
+      <>
       <div className="Login">
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Correo electrónico" />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formEmail">
+            <Form.Control type="email" name="email" placeholder="Correo electrónico" 
+              value={this.state.form.email} onChange={this.handleChange} required />
           </Form.Group>
   
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Contraseña" />
+          <Form.Group controlId="formPassword">
+            <Form.Control type="password" name="password" placeholder="Contraseña" 
+              value={this.state.form.password} onChange={this.handleChange} required />
           </Form.Group>
 
           <div className="actions">
@@ -22,6 +83,14 @@ export default class Login extends Component {
           </div>
         </Form>
       </div>
+      {showAlert && (
+        <Alert variant={this.state.alert.variant}>
+        <p>
+          {this.state.alert.message}
+        </p>
+      </Alert>
+      )}
+      </>
     );
   }
 }
