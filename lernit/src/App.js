649dc4158d08f014e6b5014b33673b7b90952821
Enrique García <garcia.enrique.torres@gmail.com>
2020-05-22 17:17:29 +0200

    Update App layout

--- lernit/src/App.js
+++ lernit/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import { FirebaseContext } from './firebase';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  static contextType = FirebaseContext
+  
+  logout() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div className="actions">
+          <Button variant="primary-lernit" className="transition-3d-hover" onClick={this.logout}>Cerrar sesión</Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
