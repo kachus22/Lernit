dbdd78445100e6e1d2f5c32a6bfa566c4d463490
Enrique García <garcia.enrique.torres@gmail.com>
2020-05-22 16:25:16 +0200

    Update provider value

--- lernit/src/firebase/provider.js
+++ lernit/src/firebase/provider.js
@@ -3,9 +3,28 @@ import Firebase from './firebase';
 import FirebaseContext from './context';
 
 export default class FirebaseProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.fb = new Firebase();
+    this.state = {
+      user: '',
+      fb: this.fb
+    }
+  }
+
+  componentDidMount() {
+    this.fb.auth.onAuthStateChanged(user => {
+      this.setState({user: user, fb: this.fb});
+      console.log(this.state);
+    });
+  }
+
+  componentDidUpdate() {
+  }
+
   render() {
     return (
-      <FirebaseContext.Provider value={new Firebase()}>
+      <FirebaseContext.Provider value={this.fb, this.state}>
         {this.props.children}
       </FirebaseContext.Provider>
     )
