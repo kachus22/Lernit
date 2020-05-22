202238808dba110b4850c6e65d5fffe47570c444
Enrique García <garcia.enrique.torres@gmail.com>
2020-05-22 11:05:56 +0200

    Initial Login Component

---
+++ lernit/src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Correo electrónico" />
+          </Form.Group>
+  
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Contraseña" />
+          </Form.Group>
+
+          <div className="actions">
+            <Button type="submit"> Iniciar Sesión </Button>
+            <Button> Crear Cuenta </Button>
+          </div>
+        </Form>
+      </div>
+    );
+  }
+}
